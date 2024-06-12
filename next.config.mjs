4c9cf46640847a5fe9a344227d8ea25521326ec6
Fadumo Aideed <fadumo.ahmed.aideed@gmail.com>
2024-06-12 21:45:44 +0100

    Finished nav

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
- 
+  // compiler: { emotion: true },
 };
 
 export default nextConfig;
