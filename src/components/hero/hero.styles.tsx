1e539337fc10d4c59251641a691928753ac5e501
Fadumo Aideed <fadumo.ahmed.aideed@gmail.com>
2025-01-12 12:37:31 +0000

    Update environment variables, enhance README, and refactor components. Removed unused images and updated hero section for better responsiveness. Improved TypeScript configuration and fixed import paths.

--- src/components/hero/hero.styles.tsx
+++ src/components/hero/hero.styles.tsx
@@ -1,47 +1,106 @@
+import { SIZE_EXTRA_SMALL } from '@/styles/breakpoints';
 import styled from '@emotion/styled';
 
 export const HeroContainer = styled.div`
+  position: relative;
+
   display: flex;
   flex-direction: row;
-  height: 700px;
   width: 100%;
-  padding: 0px 50px 0px 50px;
+  height: 700px;
 
-  @media (max-width: 600px) {
+  @media (max-width: 375px) {
     display: flex;
     flex-direction: column;
+  }
+
+  @media (min-width: 600px) {
+    display: flex;
+    flex-direction: row;
+    margin-top: 70px;
     padding: 0px;
   }
-`;
 
-export const HeroText = styled.div`
-  display: inline-block;
-  padding: 50px 0px 50px 20px;
-  h2 {
-    font-family: 'Comfortaa', sans-serif;
-    font-weight: 700;
-    font-size: 4.5rem;
-    width: 600px;
-    @media (max-width: 600px) {
-      width: auto;
-    }
-  }
-
-  h4 {
-    font-family: 'Manrope', sans-serif;
-    font-size: 1.5rem;
-    font-weight: 500;
-    padding-top: 25px;
-    width: 600px;
-  }
-
-  @media (max-width: 480px) {
-    padding: 25px 20px 50px 25px;
-    font-size: 2rem;
+  @media (min-width: 800px) {
+  height: 550px;
   }
 `;
 
 export const HeroMedia = styled.div`
-  background-color: #e5eae2;
-  border-radius: 15% 0 0 15%;
+  width: 100%;
+
+  div {
+    background-color: #e5eae2;
+    border-radius: 20px 0 0 20px;
+  }
+
+  img {
+    position: absolute;
+    bottom: 0;
+    right: 0px;
+    height: 100%;
+  }
+
+  @media (min-width: 800px) {
+    position: relative;
+    border-radius: 15% 0 0 15%;
+    flex: 1.5;
+    padding-left: 50px;
+
+    div {
+      background-color: #e5eae2;
+      position: absolute;
+      width: 80%;
+      height: 100%;
+      bottom: 0;
+      right: 0;
+      border-radius: 20px 0 0 20px;
+    }
+
+    img {
+      position: absolute;
+      bottom: 0;
+      right: 0px;
+      height: 110%;
+    }
+  }
+`;
+
+export const HeroText = styled.div`
+  flex: 1.8;
+  padding: 0px 0px 50px 50px;
+
+  h2 {
+    font-family: 'Comfortaa', sans-serif;
+    font-weight: 700;
+    font-size: 2.5rem;
+  }
+
+  h4 {
+    font-family: 'Manrope', sans-serif;
+    font-weight: 500;
+    font-size: 1rem;
+    padding-top: 25px;
+  }
+
+  @media (max-width: 375px) {
+    padding: 0px;
+  }
+
+  @media (min-width: 375px) {
+    padding: 25px 20px 50px 25px;
+    font-size: 2rem;
+  }
+
+  @media (min-width: 800px) {
+    padding: 0px 0px 50px 50px;
+
+    h2 {
+      font-size: 4.5rem;
+    }
+
+    h4 {
+      font-size: 1.5rem;
+    }
+  }
 `;
---
+++ src/styles/breakpoints.ts
@@ -0,0 +1,6 @@
+export const SIZE_EXTRA_SMALL = 576; // <576px
+export const SIZE_SMALL = 768; // 576px-768px
+export const SIZE_MEDIUM = 992; // ≥768px <992px
+export const SIZE_LARGE = 1200; // ≥992px <1200px
+export const SIZE_EXTRA_LARGE = 1200; // ≥1200px <1400px
+export const SIZE_EXTRA_EXTRA_LARGE = 1200; // ≥1400px
