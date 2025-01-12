ac7ee6b3bd2742e0f114d62ed188fb840818b127
Fadumo Aideed <fadumo.ahmed.aideed@gmail.com>
2025-01-12 12:37:31 +0000

    Update environment variables, enhance README, and refactor components. Removed unused images and updated hero section for better responsiveness. Improved TypeScript configuration and fixed import paths.

--- Types/database.types.ts
+++ Types/database.types.ts
@@ -71,12 +71,12 @@ export type Tables<
     ? R
     : never
   : PublicTableNameOrOptions extends keyof (Database['public']['Tables'] & Database['public']['Views'])
-  ? (Database['public']['Tables'] & Database['public']['Views'])[PublicTableNameOrOptions] extends {
-      Row: infer R;
-    }
-    ? R
-    : never
-  : never;
+    ? (Database['public']['Tables'] & Database['public']['Views'])[PublicTableNameOrOptions] extends {
+        Row: infer R;
+      }
+      ? R
+      : never
+    : never;
 
 export type TablesInsert<
   PublicTableNameOrOptions extends keyof Database['public']['Tables'] | { schema: keyof Database },
@@ -90,12 +90,12 @@ export type TablesInsert<
     ? I
     : never
   : PublicTableNameOrOptions extends keyof Database['public']['Tables']
-  ? Database['public']['Tables'][PublicTableNameOrOptions] extends {
-      Insert: infer I;
-    }
-    ? I
-    : never
-  : never;
+    ? Database['public']['Tables'][PublicTableNameOrOptions] extends {
+        Insert: infer I;
+      }
+      ? I
+      : never
+    : never;
 
 export type TablesUpdate<
   PublicTableNameOrOptions extends keyof Database['public']['Tables'] | { schema: keyof Database },
@@ -109,12 +109,12 @@ export type TablesUpdate<
     ? U
     : never
   : PublicTableNameOrOptions extends keyof Database['public']['Tables']
-  ? Database['public']['Tables'][PublicTableNameOrOptions] extends {
-      Update: infer U;
-    }
-    ? U
-    : never
-  : never;
+    ? Database['public']['Tables'][PublicTableNameOrOptions] extends {
+        Update: infer U;
+      }
+      ? U
+      : never
+    : never;
 
 export type Enums<
   PublicEnumNameOrOptions extends keyof Database['public']['Enums'] | { schema: keyof Database },
@@ -124,5 +124,5 @@ export type Enums<
 > = PublicEnumNameOrOptions extends { schema: keyof Database }
   ? Database[PublicEnumNameOrOptions['schema']]['Enums'][EnumName]
   : PublicEnumNameOrOptions extends keyof Database['public']['Enums']
-  ? Database['public']['Enums'][PublicEnumNameOrOptions]
-  : never;
+    ? Database['public']['Enums'][PublicEnumNameOrOptions]
+    : never;
---
+++ src/components/hero/hero.tsx
@@ -0,0 +1,27 @@
+/* eslint-disable @next/next/no-img-element */
+import React from 'react';
+import { HeroContainer, HeroText, HeroMedia } from './hero.styles';
+
+function Hero() {
+  return (
+    <HeroContainer>
+      <HeroText>
+        <h2>
+          Your <span>Personal</span> Plant Care Companion
+        </h2>
+        <h4>Learn, grow and botanify your space with this comphensive guide</h4>
+      </HeroText>
+      <HeroMedia>
+        <div></div>
+        <img
+          src={'/images/monstera-hq.png'}
+          alt="Image of monstera plant leaves"
+          max-height="950px"
+          width="auto"
+        />
+      </HeroMedia>
+    </HeroContainer>
+  );
+}
+
+export default Hero;
