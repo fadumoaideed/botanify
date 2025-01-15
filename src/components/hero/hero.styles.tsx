d9d02df132c7faf8f8defac99c93b3ba4860dbb0
Fadumo Aideed <fadumo.ahmed.aideed@gmail.com>
2025-01-15 20:25:53 +0000

    Refactor Hero component styles for improved responsiveness and layout. Added new media queries, adjusted padding, and restructured HeroMedia for better image handling and animation effects.

--- src/components/hero/hero.styles.tsx
+++ src/components/hero/hero.styles.tsx
@@ -2,15 +2,22 @@ import { SIZE_EXTRA_SMALL } from '@/styles/breakpoints';
 import styled from '@emotion/styled';
 
 export const HeroContainer = styled.div`
+  position: relative;
+  display: flex;
+  gap:20px;
+
   @media (max-width: 375px) {
-    display: flex;
     flex-direction: column;
-    padding: 50px 15px 20px 15px;
-    gap: 50px;
+  }
+
+  @media (min-width: 376px) {
+    flex-direction: column;
+    padding: 50px 10px 20px 10px;
+    padding: 0px;
+    gap: 0px;
   }
 
   @media (min-width: 600px) {
-    display: flex;
     flex-direction: row;
     margin-top: 70px;
     padding: 0px;
@@ -18,80 +25,16 @@ export const HeroContainer = styled.div`
 
   @media (min-width: 800px) {
     position: relative;
-
-    display: flex;
     flex-direction: row;
     width: 100%;
     height: 550px;
   }
 `;
 
-export const HeroMedia = styled.div`
-  .hero-media-background {
-      position: absolute;
-      background-color: #e5eae2;
-      border-radius: 30% 0 0 30%;
-      width: 100%;
-      height: 100%;
-    }
-      
-  @media (min-width: 375px) {
-    position: relative;
-    width: 100%;
-    height: 100%;
-
-    .hero-media-background {
-      position: absolute;
-      background-color: #e5eae2;
-      border-radius: 20px 0 0 20px;
-      width: 100%;
-      height: 100%;
-    }
-
-    img {
-      position: absolute;
-      width: 100%;
-      height: 100%;
-    }
-  }
-
-  @media (min-width: 800px) {
-    width: 100%;
-
-    img {
-      position: absolute;
-      bottom: 0;
-      right: 0px;
-      height: 100%;
-    }
-
-    position: relative;
-    border-radius: 15% 0 0 15%;
-    flex: 1.5;
-    padding-left: 50px;
-
-    .hero-media-background {
-      background-color: #e5eae2;
-      position: absolute;
-      width: 80%;
-      height: 100%;
-      bottom: 0;
-      right: 0;
-      border-radius: 20px 0 0 20px;
-    }
-
-    img {
-      position: absolute;
-      bottom: 0;
-      right: 0px;
-      height: 110%;
-    }
-  }
-`;
-
 export const HeroText = styled.div`
   flex: 1.8;
-  padding: 0px 0px 50px 50px;
+
+  padding: 40px 10px 20px 10px;
 
   h2 {
     font-family: 'Comfortaa', sans-serif;
@@ -106,16 +49,12 @@ export const HeroText = styled.div`
     padding-top: 25px;
   }
 
-  @media (max-width: 375px) {
-    padding: 0px;
-  }
-
-  @media (min-width: 375px) {
+  @media (min-width: 376px) {
     padding: 25px 20px 50px 25px;
     font-size: 2rem;
   }
 
-  @media (min-width: 800px) {
+  @media (min-width: 600px) {
     padding: 0px 0px 50px 50px;
 
     h2 {
@@ -125,5 +64,65 @@ export const HeroText = styled.div`
     h4 {
       font-size: 1.5rem;
     }
+  }
+
+  @media (min-width: 800px) {
+    padding: 0px 0px 50px 50px;
+  }
+`;
+
+export const HeroMedia = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  flex: 1;
+
+  width: 100%;
+  height: 450px;
+  background-color: transparent;
+  border-radius: 30% 0 0 30%;
+  position: relative;
+
+
+  .background {
+    position: absolute;
+    width: 100%;
+    height: 45%;
+    bottom: 0;
+    background-color: #e5eae2;
+    z-index: 2;
+  }
+
+  img {
+    min-width: 300px;
+    height: 110%;
+    width: 90%;
+    z-index: 3;
+    animation: scaleIn 2s ease-out;
+    transform-origin: right center;
+
+    @keyframes scaleIn {
+      from {
+        transform: scale(0.8);
+      }
+      to {
+        transform: scale(1);
+      }
+    }
+  }
+
+  border-radius: 0px;
+
+  @media (min-width: 376px) {
+    img {
+      width: 360px;
+    }
+  }
+
+  @media (min-width: 600px) {
+  }
+
+  @media (min-width: 800px) {
+    flex: 1.5;
+    padding-left: 50px;
   }
 `;
