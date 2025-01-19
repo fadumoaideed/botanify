1a317a77b205e47827b79c05cd77a080e0b6d4d7
Fadumo Aideed <fadumo.ahmed.aideed@gmail.com>
2025-01-19 20:23:17 +0000

    feat: add manifest and theme color meta tags to _document.tsx for PWA support

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,6 +13,9 @@ class MyDocument extends Document {
             rel="stylesheet"
           />
           <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&display=swap" rel="stylesheet" />
+          <link rel="manifest" href="/manifest.json" />
+          <meta name="theme-color" content="#000000" />
+          <link rel="apple-touch-icon" href="/icon-192x192.png" />
         </Head>
         <body>
           <Main />
