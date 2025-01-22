a7fcc76f05b5659e5d68575cfc1e628e78691540
Fadumo Aideed <fadumo.ahmed.aideed@gmail.com>
2025-01-22 12:01:17 +0000

    refactor: remove add-plant page and associated styles; eliminate unused components for cleaner codebase

--- src/pages/add-plant/page.tsx
+++
@@ -1,9 +0,0 @@
-import Form from '../../components/form/form';
-import styles from './page.module.css';
-export default function addPlant() {
-  return (
-    <div className={styles.container}>
-      <Form />
-    </div>
-  );
-}
