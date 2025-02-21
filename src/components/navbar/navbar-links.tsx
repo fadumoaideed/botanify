a5987c9172ce4f6c8229c9babb15a2da4ef6d46a
Fadumo Aideed <fadumo.ahmed.aideed@gmail.com>
2025-02-21 23:00:25 +0000

    feat: add mobile responsiveness to navbar and create plant care page
    - Implement mobile detection for navbar links
    - Add conditional rendering of logout link for mobile view
    - Create basic PlantCare page component
    - Reorder navbar links for improved layout

--- src/components/navbar/navbar-links.tsx
+++ src/components/navbar/navbar-links.tsx
@@ -12,6 +12,7 @@ export const NavbarLinks = () => {
    const { isAuthenticated, logout, user } = useAuth()
    const [dropdownOpen, setDropdownOpen] = useState(false)
    const dropdownRef = useRef<HTMLDivElement>(null)
+   const [isMobile, setIsMobile] = useState(false)
 
    useEffect(() => {
       const handleClickOutside = (event: MouseEvent) => {
@@ -33,30 +34,47 @@ export const NavbarLinks = () => {
       setDropdownOpen(!dropdownOpen)
    }
 
+   useEffect(() => {
+      const handleResize = () => {
+         setIsMobile(window.innerWidth < 768)
+      }
+      window.addEventListener('resize', handleResize)
+      return () => {
+         window.removeEventListener('resize', handleResize)
+      }
+   }, [])
+
    return isAuthenticated ? (
       <>
+         <Link href="/dashboard" passHref>
+            Dashboard
+         </Link>
          <Link href="/plant-care" passHref>
             Plant Care
          </Link>
 
-         <Link href="/dashboard" passHref>
-            Dashboard
-         </Link>
+         {isMobile ? (
+            <>
+               <Link href="#" onClick={logout} passHref>
+                  Log out
+               </Link>
+            </>
+         ) : (
+            <div
+               style={{ position: 'relative', marginLeft: '30px' }}
+               ref={dropdownRef}
+            >
+               <UserIconContainer onClick={toggleDropdown}>
+                  <UserIcon>{user?.firstName?.split('')[0]}</UserIcon>
+               </UserIconContainer>
 
-         <div
-            style={{ position: 'relative', marginLeft: '30px' }}
-            ref={dropdownRef}
-         >
-            <UserIconContainer onClick={toggleDropdown}>
-               <UserIcon>{user?.firstName?.split('')[0]}</UserIcon>
-            </UserIconContainer>
-
-            {dropdownOpen && (
-               <DropDownMenu>
-                  <div onClick={logout}>Log out</div>
-               </DropDownMenu>
-            )}
-         </div>
+               {dropdownOpen && (
+                  <DropDownMenu>
+                     <div onClick={logout}>Log out</div>
+                  </DropDownMenu>
+               )}
+            </div>
+         )}
       </>
    ) : (
       <>
