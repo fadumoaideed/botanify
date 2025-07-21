5ef2fd7da24e24302f213c953238305c4f20f9df
Fadumo Aideed <fadumo.ahmed.aideed@gmail.com>
2025-07-21 18:44:33 +0100

    Add error page if server fails

--- src/pages/plant-care.tsx
+++ src/pages/plant-care.tsx
@@ -1,139 +1,23 @@
-import { PlantCard } from '../components/plant-card/plant-card';
+import { plantDatabase } from '@/data/plants-database';
+import { Card } from '../components/plant-card/plant-card';
 import { useState, useEffect } from 'react';
 import { Plant } from '@/types/plant';
-import styled from '@emotion/styled';
-
-const PlantsContainer = styled.div`
-   display: grid;
-   grid-template-columns: repeat(1, 1fr);
-   grid-column-gap: 20px;
-   grid-row-gap: 40px;
-   justify-items: center;
-   width: 100%;
-
-   @media (min-width: 600px) {
-      grid-template-columns: repeat(2, 1fr);
-   }
-
-   @media (min-width: 800px) {
-      grid-template-columns: repeat(3, 1fr);
-      grid-row-gap: 70px;
-   }
-
-   @media (min-width: 1500px) {
-      grid-template-columns: repeat(4, 1fr);
-   }
-`;
-
-export const Title = styled.h3`
-   font-size: 2rem;
-   font-weight: 500;
-   font-family: Roboto, sans-serif;
-   color: rgb(58, 81, 62);
-   margin-top: 20px;
-
-   @media (min-width: 600px) {
-      margin-top: 30px;
-      font-size: 2.5rem;
-   }
-
-   @media (min-width: 800px) {
-      margin-top: 50px;
-      font-size: 3rem;
-   }
-`;
-
-export const SubTitle = styled.p`
-   font-size: 1rem;
-   font-weight: 300;
-   font-family: Roboto, sans-serif;
-   color: rgb(58, 81, 62);
-
-   @media (min-width: 600px) {
-      font-size: 1.3rem;
-   }
-`;
-
-export const Container = styled.div`
-   display: flex;
-   flex-direction: column;
-   justify-content: center;
-   margin: 10px;
-`;
-
-export const Header = styled.div`
-   display: flex;
-   flex-direction: column;
-   align-items: start;
-   justify-content: center;
-   margin: 20px 10px 50px 20px;
-`;
-
-export const Button = styled.button`
-   background-color: transparent;
-   color: #fff;
-   padding: 8px 45px;
-   border-radius: 30px;
-   border: 1px solid rgb(58, 81, 62);
-   cursor: pointer;
-   font-size: 1rem;
-   margin: 2rem 0;
-   font-weight: 500;
-   font-family: Roboto, sans-serif;
-   color: rgb(58, 81, 62);
-   margin-top: 20px;
-   align-self: center;
-
-   &:hover {
-      background-color: rgb(58, 81, 62);
-      color: #fff;
-   }
-
-   &:disabled {
-      opacity: 0.5;
-      cursor: not-allowed;
-      background-color: transparent;
-      color: rgb(58, 81, 62);
-   }
-`;
-
-export const PaginationContainer = styled.div`
-   display: flex;
-   justify-content: center;
-   align-items: center;
-   gap: 20px;
-   margin: 2rem 0;
-   flex-wrap: wrap;
-`;
-
-export const PageInfo = styled.span`
-   font-family: Roboto, sans-serif;
-   color: rgb(58, 81, 62);
-   font-size: 1rem;
-   margin: 0 10px;
-   text-align: center;
-`;
-
-export const LoadingMessage = styled.div`
-   display: flex;
-   justify-content: center;
-   align-items: center;
-   height: 200px;
-   font-family: Roboto, sans-serif;
-   color: rgb(58, 81, 62);
-   font-size: 1.2rem;
-`;
-
-export const ErrorMessage = styled.div`
-   display: flex;
-   justify-content: center;
-   align-items: center;
-   height: 200px;
-   font-family: Roboto, sans-serif;
-   color: #e74c3c;
-   font-size: 1.2rem;
-   text-align: center;
-`;
+import {
+   PlantsContainer,
+   Container,
+   Header,
+   Title,
+   SubTitle,
+   Button,
+   PaginationContainer,
+   PageInfo,
+   LoadingMessage,
+   ErrorMessage,
+   ErrorContainer,
+   ErrorImage,
+   ErrorTitle,
+   ErrorDescription
+} from '../styles/plant-care.styles';
 
 interface PaginatedResponse {
    plants: Plant[];
@@ -149,7 +33,9 @@ interface PaginatedResponse {
 
 export default function PlantCare() {
    const [plants, setPlants] = useState<Plant[]>([]);
-   const [pagination, setPagination] = useState({
+   const [pagination, setPagination] = useState<
+      PaginatedResponse['pagination']
+   >({
       currentPage: 1,
       totalPages: 1,
       totalItems: 0,
@@ -205,74 +91,68 @@ export default function PlantCare() {
       }
    };
 
-   if (loading) {
-      return (
-         <Container>
-            <Header>
-               <Title>Plant care A-Z</Title>
-               <SubTitle>
-                  Here you can find all the information you need to care for
-                  your plants.
-               </SubTitle>
-            </Header>
-            <LoadingMessage>Loading plants...</LoadingMessage>
-         </Container>
-      );
-   }
-
-   if (error) {
-      return (
-         <Container>
-            <Header>
-               <Title>Plant care A-Z</Title>
-               <SubTitle>
-                  Here you can find all the information you need to care for
-                  your plants.
-               </SubTitle>
-            </Header>
-            <ErrorMessage>
-               Error: {error}
-               <br />
-               <Button onClick={() => fetchPlants(1, 15)}>Try Again</Button>
-            </ErrorMessage>
-         </Container>
-      );
-   }
-
    return (
       <Container>
-         <Header>
-            <Title>Plant care A-Z</Title>
-            <SubTitle>
-               Here you can find all the information you need to care for your
-               plants.
-            </SubTitle>
-         </Header>
+         {loading && !error && (
+            <LoadingMessage>Loading plants...</LoadingMessage>
+         )}
+         {error && (
+            <ErrorContainer>
+               <ErrorMessage>
+                  <ErrorImage
+                     src="/images/unwatered_plant.png"
+                     alt="Unwatered Plant"
+                  />
+                  <ErrorTitle>Oops! Our plants need some water 🌱</ErrorTitle>
+                  <ErrorDescription>
+                     We're having trouble loading the plant care information
+                     right now. Just like plants need water to thrive, our
+                     servers need a moment to refresh. Please try again in a
+                     moment!
+                  </ErrorDescription>
+                  <Button onClick={() => fetchPlants(1, 15)}>
+                     Water the servers
+                  </Button>
+               </ErrorMessage>
+            </ErrorContainer>
+         )}
+         {plants.length > 0 && !error && (
+            <>
+               <Header>
+                  <Title>Plant care A-Z</Title>
+                  <SubTitle>
+                     Here you can find all the information you need to care for
+                     your plants.
+                  </SubTitle>
+               </Header>
+               <PlantsContainer>
+                  {plants.map((plant) => (
+                     <Card key={plant.id} plant={plant} />
+                  ))}
+               </PlantsContainer>
+               <PaginationContainer>
+                  <Button
+                     onClick={handlePreviousPage}
+                     disabled={!pagination.hasPreviousPage}
+                  >
+                     Previous
+                  </Button>
 
-         <PlantsContainer>
-            {plants.map((plant) => (
-               <PlantCard key={plant.id} plant={plant} />
-            ))}
-         </PlantsContainer>
+                  <PageInfo>
+                     Page {pagination.currentPage} of {pagination.totalPages}
+                     <br />
+                     Showing {plants.length} of {pagination.totalItems} plants
+                  </PageInfo>
 
-         <PaginationContainer>
-            <Button
-               onClick={handlePreviousPage}
-               disabled={!pagination.hasPreviousPage}
-            >
-               Previous
-            </Button>
-
-            <PageInfo>
-               Page {pagination.currentPage} of {pagination.totalPages}
-               <br />
-               Showing {plants.length} of {pagination.totalItems} plants
-            </PageInfo>
-
-            <Button onClick={handleNextPage} disabled={!pagination.hasNextPage}>
-               Next
-            </Button>
-         </PaginationContainer>
+                  <Button
+                     onClick={handleNextPage}
+                     disabled={!pagination.hasNextPage}
+                  >
+                     Next
+                  </Button>
+               </PaginationContainer>
+            </>
+         )}
       </Container>
    );
 }
