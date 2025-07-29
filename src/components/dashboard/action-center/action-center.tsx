d2ca5f55642fe84871663f48fa971c0d7e36c3be
Fadumo Aideed <fadumo.ahmed.aideed@gmail.com>
2025-07-29 23:55:42 +0100

    Trigger new netlify deploy

--- src/components/dashboard/action-center/action-center.tsx
+++ src/components/dashboard/action-center/action-center.tsx
@@ -1,5 +1,5 @@
 import { Container } from './action-center.styles';
 
 export const ActionCenter = () => {
-   return <Container>Action Center</Container>;
+   return <Container>Action CenterR</Container>;
 };
